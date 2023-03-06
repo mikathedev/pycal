672bfd24cde1b1581d0832c74c69c16026643628
ru23go <97406929+ru23go@users.noreply.github.com>
2023-03-06 09:52:15 -0500

    Add files via upload

---
+++ mian copy.py
@@ -0,0 +1,6 @@
+numerator = int(input(" what is the numerator"))
+denomanater = int(input("what is the denomanater"))
+num = int(input("multipled by?"))
+ans = num * numerator
+
+print(str(ans)+"/"+str(denomanater)+" has not been copied to clipboard")
