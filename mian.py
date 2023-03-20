74a2120a33644eaa3ca2ab4639f318541190ad75
ru23go <97406929+ru23go@users.noreply.github.com>
2023-03-20 10:06:37 -0400

    Delete mian.py

--- mian.py
+++
@@ -1,8 +0,0 @@
-
-import pyperclip
-numerator = int(input(" what is the numerator"))
-denomanater = int(input("what is the denomanater"))
-num = int(input("multipled by?"))
-ans = num * numerator
-pyperclip.copy(str(ans)+"/"+str(denomanater))
-print(str(ans)+"/"+str(denomanater)+" has been copied to clipboard")
