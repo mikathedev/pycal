61f7497865f27251f657bc0f0531725ddd900188
ru23go <97406929+ru23go@users.noreply.github.com>
2023-03-21 13:18:18 -0400

    Add files via upload

--- website.js
+++ website.js
@@ -1,8 +1,23 @@
 var numar = prompt("what da numaraqter")
 var denom = prompt("what da denomarater")
-var what = promt("what 1 for mfbwn 2 for mfbf and 3 for af")
+var what = prompt("what 1 for mfbwn 2 for mfbf and 3 for af")
+
+function mfbwn()
+{
+var num = prompt("multiplyd by") 
+var ans = Number(num)*Number(numar)
+alert(ans.toString,"/",denom)
+}
 
 if (what == "1"){
 
     
 }
+else {if(what == "1"){
+
+}
+
+else{if(what == "1"){
+
+
+}}}
