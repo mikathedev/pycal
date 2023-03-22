573563c607763756486944f8148718984643f514
ru23go <97406929+ru23go@users.noreply.github.com>
2023-03-22 08:18:24 -0400

    Add files via upload

--- website.js
+++ website.js
@@ -6,15 +6,29 @@ function mfbwn()
 {
 var num = prompt("multiplyd by") 
 var ans = Number(num)*Number(numar)
-alert(ans.toString,"/",denom)
+alert(ans+"/"+denom)
 }
 
 function mfbf(){
     var numar2 = prompt("second numaraqter")
     var denom2 = prompt("second denomarater")
-    if(denom2 == denom2){
-        var ansnuma = Number(numar)*Number(numar2)
+    var ansnuma = Number(numar)*Number(numar2)
+    var ansdenom = Number(denom)*Number(denom2)
+    alert(ansnuma+"/"+ansdenom)
+    
+}
+
+function af(){
+    var numar2 = prompt("second numaraqter")
+    var denom2 = prompt("second denomarater")
+    if(denom2 == denom){
+        ansnuma = Number(numar)+Number(numar2)
+        ansdenom =denom
     }
+    else{
+        var ansdenom = denom2*denom
+    }
+
 }
 
 if (what == "1"){
@@ -22,11 +36,17 @@ if (what == "1"){
     mfbwn()
     
 }
-else {if(what == "1"){
+else {if(what == "2"){
+    mfbf()
+    
+    
+}
+else{if(what == "3"){
 
+af()
+}
 }
 
-else{if(what == "1"){
 
 
-}}}
+}
