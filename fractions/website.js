5f37eba190ef0781ddd5862b14c666535c886331
ru23go <yowest11@hotmail.com>
2023-04-04 10:03:11 -0400

    aaa

--- fractions/website.js
+++ fractions/website.js
@@ -23,7 +23,7 @@ function af(){
     var denom2 = prompt("second denomarater")
     if(denom2 == denom){
         ansnuma = Number(numar)+Number(numar2)
-        ansdenom =denom
+        ansdenom = denom
     }
     else{
         var ansdenom = denom2*denom
@@ -48,8 +48,25 @@ af()
 }
 function cal(){
     var how = prompt("1 to add 2 to subtract 3 to multiply 4 to divide")
-    
-    
+var num1 = prompt("Enter first number")
+var num2 = prompt("Enter second number")
+if(how == "1"){
+    var ans = Number(num1)+Number(num2)
+    alert(ans)
+}
+else if(how == "2"){
+    var ans = Number(num1)-Number(num2)
+    alert(ans)
+}
+else if(how == "3"){
+    var ans = Number(num1)*Number(num2)
+    alert(ans)
+}
+else if(how == "4"){
+    var ans = Number(num1)/Number(num2)
+    alert(ans)
+}
+
     
 }
 
